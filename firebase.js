3414e28b01ec0afe704950a597231d52416a8684
amir Zhaksylykov <amirzhaksylykov@amirs-MacBook-Pro.local>
2024-07-10 11:48:25 +0500

    Initial app commit

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-qLpeoPLQ7cuMGC-wL2kGIK08N0WdgdU",
+  authDomain: "react-notes-a6397.firebaseapp.com",
+  projectId: "react-notes-a6397",
+  storageBucket: "react-notes-a6397.appspot.com",
+  messagingSenderId: "396214566402",
+  appId: "1:396214566402:web:b783ddfd22e7da7eb9f11b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const notesCollection = collection(db, "notes")
